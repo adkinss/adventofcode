1f5815598aced2d3f803fe355e587f46d96d8e32
Scott Adkins <adkinss@gmail.com>
2024-12-02 10:16:16 -0500

    Move conversion to integer to the transformation function.

--- 2024/src/day_1.py
+++ 2024/src/day_1.py
@@ -4,11 +4,8 @@ from utils import read_input
 
 
 def transformer(line):
-    """
-    Scan the line and return both numbers found.
-    Example: "3   4" returns 3 and 4
-    """
-    return line.split()
+    string_list = line.split()
+    return [int(s) for s in string_list]
 
 
 input = read_input("1", transformer, example=True)
@@ -16,24 +13,24 @@ first = sorted([i[0] for i in input])
 last = sorted([i[1] for i in input])
 [distance_p1, similarity_p1] = [0, 0]
 for idx, f in enumerate(first):
-    distance_p1 += abs(int(f) - int(last[idx]))
+    distance_p1 += abs(f - last[idx])
     count = 0
     for l in last:
         if f == l:
             count += 1
-    similarity_p1 += int(f) * count
+    similarity_p1 += f * count
 
 input = read_input("1", transformer, example=False)
 first = sorted([i[0] for i in input])
 last = sorted([i[1] for i in input])
 [distance_p2, similarity_p2] = [0, 0]
 for idx, f in enumerate(first):
-    distance_p2 += abs(int(f) - int(last[idx]))
+    distance_p2 += abs(f - last[idx])
     count = 0
     for l in last:
         if f == l:
             count += 1
-    similarity_p2 += int(f) * count
+    similarity_p2 += f * count
 
 print(f'Part 1 Example:\t{distance_p1}\t\tExpecting: 11\t\tIs Correct? {distance_p1 == 11}')
 print(f'Part 1 Actual:\t{distance_p2}\t\tExpecting: 1879048\tIs Correct? {distance_p2 == 1879048}')
